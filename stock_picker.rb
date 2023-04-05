e2e65ed558a3eaf23bde5dbb04f6338ac3226347
krekoster <72513456+krekoster@users.noreply.github.com>
2023-04-05 22:42:22 +0200

    Update stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,17 @@
 def stock_picker(prices)
   result = []
-  result.push(prices.last(prices.length - 1).max)
-  result.push(prices.last(prices.length - 1).min)
-  result
+  difference = 0
+  prices.each_with_index  do |price_current, index_current| 
+    prices.last(prices.length-1).each_with_index do |price_next, index_next| 
+      if price_next - price_current > difference && index_next > index_current
+        difference = price_next - price_current 
+        result[0] = index_current + 1
+        result[1] = index_next + 2
+      end
+        
+    end
+  end
+  return result
 end
 
-puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+print stock_picker ([ 17, 1, 3, 6, 9, 15, 8, 6, 288, 1,  345, 10])
